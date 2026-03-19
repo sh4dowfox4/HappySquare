8422c59404077909a9fdc15480464d4cf1a3c067
sh4dowfox4 <amerikapendosiya@gmail.com>
2026-03-19 18:47:13 +0500

    HappySquare 0.0.4
    Добавлен новый вид врагов - стрелки
    
    Добавлен debug mode
    
    Добавлены sfx звуки и музыка
    
    Исправление багов

---
+++ нейронка/entity.cpp
@@ -0,0 +1,39 @@
+#include "entity.h"
+
+Entity::Entity(Vector2 p, float w, float h, int hp, Color c)
+    : pos(p), width(w), height(h), health(hp), maxHealth(hp), color(c), isActive(true) {}
+
+void Entity::update(float deltaTime) {}
+
+void Entity::draw() const {
+    DrawRectangle(pos.x, pos.y, width, height, color);
+}
+
+bool Entity::checkCollision(const Entity& other) const {
+    return (pos.x < other.pos.x + other.width &&
+            pos.x + width > other.pos.x &&
+            pos.y < other.pos.y + other.height &&
+            pos.y + height > other.pos.y);
+}
+
+void Entity::takeDamage(int damage) {
+    health -= damage;
+    if (health <= 0) isActive = false;
+}
+
+bool Entity::isAlive() const {
+    return health > 0 && isActive;
+}
+
+Rectangle Entity::getBounds() const {
+    return {pos.x, pos.y, width, height};
+}
+
+Vector2 Entity::getPosition() const {
+    return pos;
+}
+
+int Entity::getHealth() const { return health; }
+int Entity::getMaxHealth() const { return maxHealth; }
+bool Entity::getIsActive() const { return isActive; }
+void Entity::setIsActive(bool active) { isActive = active; }
--- нейронка/main.cpp
+++ нейронка/main.cpp
@@ -3,7 +3,7 @@
 bool g_debugMode = false;
 
 int main() {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "HappySquare 0.0.4");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "HappySquare 0.0.3");
     SetTargetFPS(TARGET_FPS);
 
     Game game;
